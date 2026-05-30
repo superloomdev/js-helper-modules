eed0d16adf0ec0a70523f4f37f36b9e06e042587
sj00 <mail@shivanshujindal.com>
2026-05-30 18:31:00 +0530

    fix(sql): update Utils loader call pattern in test loaders
    - sql-postgres, sql-mysql, sql-sqlite: require('helper-utils')() → require('helper-utils')(Lib, {})

--- src/helper-modules-server/js-server-helper-sql-postgres/_test/loader.js
+++ src/helper-modules-server/js-server-helper-sql-postgres/_test/loader.js
@@ -49,7 +49,7 @@ module.exports = function loader () {
 
   // ==================== HELPER MODULES ============================= //
 
-  Lib.Utils = require('helper-utils')();
+  Lib.Utils = require('helper-utils')(Lib, {});
   Lib.Debug = require('helper-debug')(Lib, config_debug);
   Lib.Instance = require('helper-instance')(Lib, {});
 
