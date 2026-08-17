b6f9c86a370a796339c4082c1481efba3f576336
Shivanshu Jindal <mail@shivanshujindal.com>
2026-08-18 05:12:14 +0530

    refactor: remove contact-family modules for architecture redesign
    Moving to basic/extended nomenclature. Existing work backed up to __dev__/wip/0097-backup. Phone packages already published to registry will be superseded by new versions.

--- src/helper-modules-core/js-helper-contact-phone-adapter-basic/adapter.config.js
+++
@@ -1,7 +0,0 @@
-// Info: Configuration defaults for helper-contact-phone-adapter-basic.
-// The basic adapter needs no configuration - the country data is generated
-// and committed. This file exists per the Universal Companion Files rule.
-'use strict';
-
-
-module.exports = {};
--- src/helper-modules-core/js-helper-contact-phone-adapter-basic/adapter.errors.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * Error catalog for helper-contact-phone-adapter-basic.
- * The basic adapter is stateless and does not produce operational errors.
- * The catalog exists per the Universal Companion Files rule.
- */
-
-module.exports = Object.freeze({});
